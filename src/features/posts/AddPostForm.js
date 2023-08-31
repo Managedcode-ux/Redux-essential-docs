d2797d4edb96737a0338d87223e829ed0fa63857
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2023-08-31 18:02:12 +0530

    Async logic completed

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
+import {addNewPost} from "./postsSlice";
 
 import { postAdded } from './postsSlice'
 export const AddPostForm = () => {
 	const [title, setTitle] = useState('')
 	const [content, setContent] = useState('')
 	const [userId, setUserId] = useState('')
+	const [addRequestStatus, setAddRequestStatus] = useState('idle')
 	const dispatch = useDispatch()
 
 	const users = useSelector(state => state.users)
 	const onTitleChanged = e => setTitle(e.target.value)
 	const onContentChanged = e => setContent(e.target.value)
 	const onAuthorChanged = e => setUserId(e.target.value)
-	const onSavePostClicked = () => {
-  if (title && content) {
-    dispatch(postAdded(title, content, userId))
-    setTitle('')
-    setContent('')
-  }
+
+	const canSave = [title,content,userId].every(Boolean) && addRequestStatus === 'idle'
+	const onSavePostClicked = async() => {
+		if(canSave){
+			try{
+				setAddRequestStatus('pending')
+				console.log(await dispatch(addNewPost({title,content,user:userId})).unwrap())
+				setTitle('')
+				setContent('')
+				setUserId('')
+			}catch (err){
+				console.error("Failed to save the post: ",err)
+			}finally {
+				setAddRequestStatus('idle')
+			}
+		}
 }
-const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
 const usersOptions = users.map(user => (
 	<option key={user.id} value={user.id}>
